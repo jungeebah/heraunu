1baa9ce65325fa246a6c01d589aeba5341bd323d
jungee <jungebah@gmail.com>
2021-01-04 00:02:31 -0800

    added the button

--- src/components/About/contactForm/contactForm.jsx
+++ src/components/About/contactForm/contactForm.jsx
@@ -1,17 +1,28 @@
 import React from "react";
-import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
-import Icon from '@material-ui/core/Icon';
 import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import ClearIcon from '@material-ui/icons/Clear';
-import './contactForm.css';
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import Typography from '@material-ui/core/Typography';
+// import './contactForm.css';
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 import emailjs from 'emailjs-com';
 
+const useStyles = makeStyles((theme) => ({
+    dialog: {
+        maxWidth: '400px'
+    },
+    inputButton: {
+        backgroundColor: theme.palette.secondary.main,
+        borderRadius: '5px',
+        color: '#fff',
+    }
+}))
+
 const ContactForm = () => {
+    const classes = useStyles();
+    const theme = useTheme();
+    const mobile = useMediaQuery(theme.breakpoints.down("xs"));
     const [open, setOpen] = React.useState(false);
 
     const handleClickOpen = () => {
@@ -22,6 +33,7 @@ const ContactForm = () => {
         setOpen(false);
     };
     const sendEmail = (e) => {
+        setOpen(false);
         e.preventDefault();
 
         emailjs.sendForm('service_prvz249', 'template_c9pxy6p', e.target, 'user_IRVvPDXeXMc7xMV6eB051')
@@ -36,26 +48,76 @@ const ContactForm = () => {
             <Button variant="contained" color="secondary" onClick={handleClickOpen}>
                 Contact
                         </Button>
-            <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-                <DialogTitle id="form-dialog-title">Message In A bottle</DialogTitle>
-                <form action="#" name="contact_form" onSubmit={sendEmail}>
-                    <input type="hidden" name="contact_number" />
-                    <label >Name</label>
-                    <input name="name" type="text" required placeholder="John" />
-                    <br />
-                    <label >Email</label>
-                    <input name="email" type="email" required placeholder="you@domain.com" />
-                    <br />
-                    <label for="message">Message</label>
-                    <textarea name="message" />
-                    <br />
-                    <br />
-                    <div className="button--container">
-                        <input type="submit" value="Submit"></input>
-                    </div>
-                </form>
+            <Dialog
+                open={open}
+                classes={{ paperWidthSm: classes.dialog }}
+                onClose={handleClose}
+                aria-labelledby="form-dialog-title">
+                <Grid container spacing={2}>
+                    <Grid item xs={4}>
+                        <Grid container
+                            spacing={0}
+                            direction="column"
+                            alignItems="center"
+                            justify="center"
+                            style={{ minHeight: '220px' }}>
+                            <Grid item xs={12}>
+
+                                <Typography variant={mobile ? 'body2' : 'body1'}>
+                                    FeedBack
+                        </Typography>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={8}>
+
+                        <form action="#" name="contact_form" onSubmit={sendEmail}>
+                            <input type="hidden" name="contact_number" />
+                            <Grid container spacing={2}>
+                                <Grid item xs={12}>
+                                    <Grid container >
+                                        <Grid item xs={12}>
+                                            <label >Name</label>
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                            <input name="name" type="text" required placeholder="Shankar" />
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                                <br />
+                                <Grid item xs={12}>
+                                    <Grid container >
+                                        <Grid item xs={12}>
+                                            <label >Email</label>
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                            <input name="email" type="email" required placeholder="you@domain.com" />
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                                <br />
+                                <Grid item xs={12}>
+                                    <Grid container >
+                                        <Grid item xs={12}>
+                                            <label for="message">Message</label>
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                            <textarea name="message" />
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                                <br />
+                                <br />
+                                <div className="button--container">
+                                    <input type="submit" value="Submit"
+                                        className={classes.inputButton}></input>
+                                </div>
+                            </Grid>
+                        </form>
+                    </Grid>
+                </Grid>
             </Dialog>
-        </div>
+        </div >
     );
 };
 
