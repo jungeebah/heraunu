9bc695097b7e31bb72f932476e11e2f7e5a553c7
jungee <jungebah@gmail.com>
2021-01-11 08:45:07 -0800

    slight modification

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,17 +3,12 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
 import { fade, makeStyles } from '@material-ui/core/styles';
-import Grow from "@material-ui/core/Grow";
-import MenuIcon from '@material-ui/icons/Menu';
 import Brightness5Icon from "@material-ui/icons/Brightness5";
 import Brightness4Icon from "@material-ui/icons/Brightness4";
 import Tooltip from '@material-ui/core/Tooltip';
-import SearchIcon from '@material-ui/icons/Search';
-import Autocomplete from '../AutoComplete/Autocomplete';
-import MobileAutocomplete from '../AutoComplete/MobileAutocomplete/MobileAutocomplete'
-import { borderRadius } from 'polished';
+import Autocomplete from '../Autocomplete/Autocomplete'
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
