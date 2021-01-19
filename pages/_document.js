2e049538e2c3ef4afe3ffd1b19cf7cc4708ac550
jungee <jungebah@gmail.com>
2021-01-18 23:09:46 -0800

    added discription

--- pages/_document.js
+++ pages/_document.js
@@ -14,10 +14,7 @@ export default class MyDocument extends Document {
                     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
                     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
                     <meta name="theme-color" content="#000000" />
-                    <meta
-                        name="description"
-                        content="Nepali Movies"
-                    />
+                    
                     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
                     <link rel="manifest" href="/manifest.json" />
                     <meta name="theme-color" content={LightTheme.palette.primary.main} />
