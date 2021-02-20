c0d0d1bb476cf85ccb532bb5141481cb95557d08
jungee <jungebah@gmail.com>
2021-02-20 07:26:58 -0800

    needed to add youtube image

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     images: {
-        domains: ['storage.googleapis.com', 'm.media-amazon.com', 'image.tmdb.org'],
+        domains: ['storage.googleapis.com', 'm.media-amazon.com', 'image.tmdb.org', 'i.ytimg.com'],
     },
 }
