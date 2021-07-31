cdd86d6d5251eecd73e20995da4f37c1dd958462
jungeebah <herauna@herauna.com>
2021-07-30 18:12:39 -0700

    youtube image

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     images: {
-        domains: ['storage.googleapis.com', 'm.media-amazon.com', 'image.tmdb.org', 'i.ytimg.com'],
+        domains: ['storage.googleapis.com', 'm.media-amazon.com', 'image.tmdb.org', 'i.ytimg.com','img.youtube.com'],
     },
 }
