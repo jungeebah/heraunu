1fdc98dee203c3e4c585c13987227cd0acfea951
jungeebah <herauna@herauna.com>
2021-08-03 23:27:28 -0700

    added genre redux

--- store.js
+++ store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import allSearchReducer from './lib/slice/search';
 import movieDataReducer from './lib/slice/moviesDataSlice';
 import personDataReducer from './lib/slice/personUserSlice';
+import genreDataReducer from './lib/slice/genreDataSlice';
 import youtubeDataReducer from './lib/slice/youtubeDataSlice';
 import searchResultDataReducer from './lib/slice/searchResult';
 
@@ -10,6 +11,7 @@ export default configureStore({
         allSearch: allSearchReducer,
         movieData: movieDataReducer,
         personData: personDataReducer,
+        genreData: genreDataReducer,
         youtubeData: youtubeDataReducer,
         searchResultData: searchResultDataReducer,
     },
