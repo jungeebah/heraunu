94da0a6e33872af6c4a6f4067ef49cbd9fd97245
anityam <89035703+anityam@users.noreply.github.com>
2024-06-23 19:51:46 -0400

    remove optimization

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     images: {
         domains: ['storage.googleapis.com', 'm.media-amazon.com', 'image.tmdb.org', 'i.ytimg.com','img.youtube.com'],
+        unoptimized: false,
     },
 }
